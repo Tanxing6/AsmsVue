149c6a2af777958e7bd5a425fec8ea6b0d89af93
dyk <2718853774@qq.com>
2021-07-13 22:16:09 +0800

    邓峄楷提交

--- src/App.vue
+++ src/App.vue
@@ -32,20 +32,6 @@
 
 					</el-submenu>
 					<el-submenu index="2">
-					  <template #title><i class="el-icon-message"></i>客户</template>
-					  <el-menu-item-group >
-						  <router-link to="/Kehu">
-					    <el-menu-item index="2-1">客户资料</el-menu-item>
-						</router-link>
-					  </el-menu-item-group>
-					  <el-menu-item-group >
-						   <router-link to="/Cheliang">
-					    <el-menu-item index="2-2">车辆资料</el-menu-item>
-						 </router-link>
-					  </el-menu-item-group>
-					  <el-menu-item-group >
-						  <router-link to="/Huiyuan">
-					    <el-menu-item index="2-4">会员管理</el-menu-item>
 						<template #title><i class="el-icon-message"></i>客户</template>
 						<el-menu-item-group>
 							<router-link to="/Kehu">
@@ -57,11 +43,11 @@
 								<el-menu-item index="2-2">车辆资料</el-menu-item>
 							</router-link>
 						</el-menu-item-group>
-						<el-menu-item-group>
+						<!-- <el-menu-item-group>
 							<router-link to="/Jiage">
 								<el-menu-item index="2-3">会员价格</el-menu-item>
 							</router-link>
-						</el-menu-item-group>
+						</el-menu-item-group> -->
 						<el-menu-item-group>
 							<router-link to="/Huiyuan">
 								<el-menu-item index="2-4">会员管理</el-menu-item>
